4bc0324eeee808dba4d16029ff548218b6479da0
Igor <sowinski.igor@gmail.com>
2019-04-12 16:11:20 +0200

    backend 1# wrote test for RepositoryFacade getLastRepo()

--- src/test/java/com/example/igor/sowinski/Allegro/Repo/domain/model/RepositoryFacadeTest.java
+++ src/test/java/com/example/igor/sowinski/Allegro/Repo/domain/model/RepositoryFacadeTest.java
@@ -1,12 +1,36 @@
 package com.example.igor.sowinski.Allegro.Repo.domain.model;
 
+import com.example.igor.sowinski.Allegro.Repo.api.dto.RepositoryNameDto;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class RepositoryFacadeTest {
 
+
+
+
     @Test
-    public void getLatestRepo() {
+    public void shouldReturnLatestRepositoryNameDto() {
+        //given
+        RepositoryService repositoryService = new RepositoryService("https://api.github.com/users/IgorSow/repos");
+        RepositoryFacade repositoryFacade = new RepositoryFacade(repositoryService);
+
+        List<Repository> repositoryList = repositoryService.getRepositoryList();
+        List<Repository> orderListByUpdateDate = repositoryService.orderListByUpdateDate(repositoryList);
+        Repository latestRepo = repositoryService.getLatestRepo(orderListByUpdateDate);
+
+
+        //when
+
+        RepositoryNameDto latestRepoDto = repositoryFacade.getLatestRepo();
+        //then
+
+        Assert.assertEquals(latestRepo.getName(), latestRepoDto.getNameRepository());
+
+
     }
 }
