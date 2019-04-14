2d9037328375ed8e942fabc6bc8ce3839ae04f34
Igor <sowinski.igor@gmail.com>
2019-04-14 22:27:56 +0200

    frontend 1# add all needet components, 2# have to style

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {MainPageComponent} from './pages/main-page/main-page.component';
 import {HttpClientModule} from '@angular/common/http';
+import { BackgroundComponent } from './components/background/background.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        MainPageComponent
+        MainPageComponent,
+        BackgroundComponent
     ],
     imports: [
         BrowserModule,
